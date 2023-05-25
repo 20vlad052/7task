80b57f750b3467adb88f380ed76b34db1e50e01a
Ralenskiy Vladislav <mr.ralesnkiy@mail.ru>
2023-05-25 12:44:05 +0300

    created an html css template

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import{style}from"./logo.css"
+function App() {
+  return (
+    <>
+    <div className="logo">
+    <div className="logotip">
+        <p>LOGO</p>
+    </div>
+    </div>
+    <div className="body">
+            <p className="linkWrap">
+            <a href="/#">Статьи</a>
+            </p>
+            <p className="linkWrap">
+              <a href="/#">Тесты </a>
+            </p>
+            <p className="linkWrap">
+              <a href="/#">Игры </a>
+            </p>
+      </div>
+
+      <div className="footer">
+        <p className="footer-text">BANNER</p>
+      </div>
+    
+</>
+  );
+}
+
+export default App;
